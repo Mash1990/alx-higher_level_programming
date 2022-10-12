9503e325af32f9205897427b743c59d5a0bda604
Brian <brian.macharia731@gmail.com>
2022-10-12 16:21:34 +0300

    python-classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square Class
+
+A Square Class
+
+"""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """__init__
+
+        The __init__ method initializes the size value of the square.
+
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+
+        Raises:
+            TypeError: If `size` type is not `int`.
+
+            ValueError: If `size` is less than `0`.
+
+        """
